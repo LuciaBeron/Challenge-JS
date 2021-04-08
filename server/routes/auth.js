9afe4499f224a35b7bfef2a9a3c33c219c94ad06
luciaberon <luciaberonwaingart@hotmail.com>
2021-04-08 09:17:04 -0300

    Created routes folder

---
+++ server/routes/auth.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../config/db');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+
+router.post('/register',(req,res) => {
+
+    const password = req.body.password;
+    const email = req.body.email;
+
+    bcrypt.hash(password, saltRounds, (err, hash) => {
+        if (err) {
+            console.log(err);
+        }
+        db.query("INSERT INTO users (password, email) VALUES (?,?)",
+        [hash,email],
+        (err,result) => {
+            if (err) {
+                console.log(err);
+                res.send({message: "Couldn't create account"})
+            } else {
+            res.send({message: "Account created!"})
+            }
+        })
+        
+    })
+
+
+})
+
+
+router.post('/login',(req,res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    db.query(
+        "SELECT * FROM users WHERE email = ?",
+        [email],
+        (err,result) => {
+            if (err) {
+                res.send({err: err});
+            } if (result.length > 0) {
+                bcrypt.compare(password, result[0].password, (error,response) => {
+                    if (response) {
+                        req.session.user = result;
+                        res.send(result);
+                    } else {
+                        res.send({message: "Wrong password"});
+                    }
+                })                
+            } else {
+                res.send({message: "Account not found"})
+            }
+    });    
+});
+
+
+router.get('/login', (req,res) => {
+    if (req.session.user) {
+        res.send({loggedIn: true, user: req.session.user});
+    } else {
+        res.send({loggedIn: false});
+    }
+})
+
+router.get('/logout', (req,res) => {
+    req.session.destroy();
+    
+})
+
+module.exports = router;
---
+++ server/routes/links.js
@@ -0,0 +1,103 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../config/db');
+
+router.get('/balance',(req,res) => {
+    const id = req.session.user[0].id;
+
+    db.query("SELECT SUM(amount) as deposits FROM records WHERE userID = ? AND type_of_operation = 'deposit'", [id],
+    (err,deposits) => {
+        if (err) {
+            console.log(err);
+        } 
+
+        db.query("SELECT SUM(amount) as substract FROM records WHERE userID = ? AND type_of_operation = 'withdrawal'", [id],(error,withdrawals) => { 
+            if (error) {
+                console.log(error);
+            } 
+
+            const sum = (add, subst) =>{
+                if (add == null) {
+                    add = 0;
+                }
+                if (subst == null) {
+                    subst = 0;
+                }
+                return (add-subst);
+            }
+                    
+            res.send({balance:  sum(deposits[0].deposits, withdrawals[0].substract)});
+            
+        })
+
+    })
+
+})
+
+
+router.post('/manage', (req,res) => {
+    const type = req.body.type;
+    const date = req.body.date;
+    const amount = req.body.amount;
+    const id = req.session.user[0].id;
+
+    db.query("INSERT INTO records (type_of_operation,amount,operationDate, userID) values (?,?,?,?)",[type,amount,date,id],
+    (err,result) => {
+        if (err) {
+            console.log(err);
+        }
+        
+    })
+})
+
+router.get('/home', (req,res) => {
+    const id = req.session.user[0].id;
+    db.query("SELECT type_of_operation, amount FROM records WHERE userID = ? LIMIT 10", [id],
+    (error,result) => {
+        if (error) {
+            console.log(error);
+        }
+        res.send({operations: result});
+    })
+})
+
+router.get('/operations', (req,res) => {
+    const id = req.session.user[0].id;
+    db.query("SELECT type_of_operation, amount,operationID, operationDate FROM records WHERE userID = ?", [id],
+    (error,result) => {
+        if (error) {
+            console.log(error);
+        }
+        res.send({operations: result});
+    })
+
+})
+
+router.delete('/operations',(req,res) => {
+    const id = req.body.id;
+    console.log("ID??: ",id);
+    db.query("DELETE FROM records WHERE operationID = ?", [id], (err,result) => {
+        if (err) {
+            console.log(err);
+        }
+        res.send("Deleted");
+    })
+})
+
+router.put('/operations',(req,res) => {
+    const id = req.body.id;
+    const amount = req.body.form.amount;
+    const date = req.body.form.date;
+    
+    db.query("UPDATE records SET amount = ?, operationDate = ? WHERE operationID = ?", [amount,date,id]
+    ,(err,result) => {
+        if (err) {
+            console.log(err);
+        }
+        res.send("changes done");
+
+    })
+})
+
+
+module.exports = router;
